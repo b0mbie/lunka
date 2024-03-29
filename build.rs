8903b2db6222c151329825ac24bb44070cb1faab
akabomb-prog <69766525+b0mbie@users.noreply.github.com>
2024-03-30 01:37:46 +0200

    Prepare library

--- build.rs
+++
@@ -1,6 +0,0 @@
-//! Build script to link against `lua54`.
-
-fn main() {
-	println!("cargo:rustc-link-search={}/build", env!("LUA_DIR"));
-	println!("cargo:rustc-link-lib=lua54");
-}
--- test.lua
+++
@@ -1,7 +0,0 @@
---- @diagnostic disable: undefined-global
--- Test code for Lua to run.
-
-print("I'm difficult-icult", ...)
-rs.print("Hello, Rust!", ...)
-
-rs.raise("bee") -- bug
